05c191835a07eb899c3c6162c963986e68d3637c
LuanRodriguesep <luanrodriguesep@yahoo.com.br>
2021-10-21 04:06:30 -0300

    criado arquivo de service e controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
-import express from "express";
+import "dotenv/config";
+import express, { request } from "express";
 
 const app = express();
 
-app.listen(4000, () => console.log(`--> Server is running on PORT 4000`))
+app.get("/github", (request, response) => {
+  response.redirect(`http://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
+
+app.listen(4000, () => console.log(`--> Server is running on PORT 4000`));
